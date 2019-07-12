469b064a988047765f46091c742d7ab71c921e4e
python240419 <51232248+python240419@users.noreply.github.com>
2019-07-12 10:58:22 +0300

    Update set_exercise.py

--- set_exercise.py
+++ set_exercise.py
@@ -1,4 +1,19 @@
 
+# Set is an unsorted unique list
+a = set()
+print(type(a)) # <class 'set'>
+a = {}
+print(type(a)) # <class 'dict'>
+a = {5}
+print(type(a)) # <class 'set'>
+a = {}
+a.add(1)
+print(a) # {1}
+a.add(1)
+print(a) # {1} -- still the same because no duplciations in set
+list1 = [1, 1 , 4 , 5 , 6 , 7, 1, 4, -3]
+print(set(list1)) # {1, 4, 5, 6, 7, -3}
+
 # create dictionary with duplicated values
 # 1 : 'o' one
 # 2 : 't' two
@@ -10,4 +25,4 @@
 # write a loop to display how many times
 # each letter repeats itself
 
-# 
+# hint : use list, set, count

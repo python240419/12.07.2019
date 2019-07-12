c01c8f60a8889d7b7b1e20e8f1c76a678ae6ff27
python240419 <51232248+python240419@users.noreply.github.com>
2019-07-12 12:09:41 +0300

    Update files_1.py

--- files_1.py
+++ files_1.py
@@ -1,23 +1,29 @@
 
 # FILES
-
 f1 = open('c:/itay/hello.txt') # open file for read - default
 
 print(f1)
+print(f1.tell()) # tell me where i am in the file
+listOfLines = f1.readlines() # read all the lines into a list
+                             # not for very large files
 
-listOfLines = f1.readlines() # not for very large files
-
+print(f1.tell()) # tell me where i am in the file
 for line in listOfLines:
-    print(line[:-1])
+    print(line, end="")
 
-print(listOfLines) # ['hello\n', 'python\n', 'world!']
+print('\n', listOfLines) # ['hello\n', 'python\n', 'world!']
 
-f1.seek(0) # moves file cursor to begining of the file
+f1.seek(1) # moves file cursor to begining of the file
 
 for line in f1.readlines():
-    print(line[:-1])
+    print(line, end="")
 
 f1.close()
 
-#for line in f1.readlines():
+#for line in f1(): -- automatically does readlines()
 #    print(line)
+
+with open('c:/itay/hello.txt') as f1:
+    for line in f1:
+        print(line, end="")
+    # because with -- here the open will be closed

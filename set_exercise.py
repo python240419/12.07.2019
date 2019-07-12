4b8cd84b218e5afa409ce1f2da83a2f044acb4b8
python240419 <51232248+python240419@users.noreply.github.com>
2019-07-12 10:52:23 +0300

    Create set_exercise.py

---
+++ set_exercise.py
@@ -0,0 +1,13 @@
+
+# create dictionary with duplicated values
+# 1 : 'o' one
+# 2 : 't' two
+# 3 : 't'
+# 4 : 'f'
+# 5 : 'f'
+# ... 10
+
+# write a loop to display how many times
+# each letter repeats itself
+
+# 
